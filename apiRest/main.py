f00d4f20ea77742affa385fe6b9aa832816e7c50
humbertoCerezo <144205302+humbertoCerezo@users.noreply.github.com>
2024-02-15 20:43:47 +0000

    Se añade función con validación de All fields required y el error 400

--- apiRest/main.py
+++ apiRest/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, EmailStr
 from typing import Optional
 import re
 
@@ -12,20 +12,30 @@ class User(BaseModel):
     name: str
     email: str
 
+
 #Diccionario para guardar datos
 users = []
 
 #Expresión regular para el Email
 email_regex = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
+#Función para validar que todos los campos no estén vacíos
+def validate_user_fields(user: User):
+    if any(value == "" for value in user.dict().values()):
+        raise HTTPException(status_code=400, detail="All fields are required")
+
+
+#Endpoint índice
 @app.get("/")
 def index():
     return {"message": "Hola, este es el índice"}
 
+#Endpoint all users
 @app.get("/all-users")
 def all_users():
     return users
 
+#Endpoint user detail
 @app.get("/user-detail/{user_id}")
 def user_detail(user_id: int):
     for user in users:
@@ -33,14 +43,17 @@ def user_detail(user_id: int):
             return user
     raise HTTPException(status_code=404, detail="User Not Found")
 
-@app.post("/create-users")
+#Endpoint create user
+@app.post("/create-user")
 def create_user(user: User):
+    validate_user_fields(user)
+    # Verifica si el email es válido
     if not email_regex.match(user.email):
-        raise HTTPException(status_code =401, detail=f"The following email:  '{user.email}' is not valid")
-    else:
-        users.append(user)
-        return {"message": "User created succesfuly"}
-    
+        raise HTTPException(status_code=400, detail=f"The following email: '{user.email}' is not valid")
+    users.append(user)
+    return {"message": "User created successfully"}
+
+#Endpoint delete user
 @app.delete("/delete-user/{user_id}")
 def delete_user(user_id: int):
     for index, user in enumerate(users):
@@ -49,8 +62,10 @@ def delete_user(user_id: int):
             return {"message": "User succesfuly deleted"}
     raise HTTPException(status_code=404, detail="User Not Found")
 
+#Endpoint update user
 @app.put("/update-user/{user_id}")
 def update_user(user_id: int, updated_user: User):
+    validate_user_fields(updated_user)
     for index, user in enumerate(users):
         if user.id == user_id:
             if not email_regex.match(updated_user.email):
@@ -60,6 +75,8 @@ def update_user(user_id: int, updated_user: User):
                 users[index].email = updated_user.email
                 return {"message": "User succesfuly updated"}
     raise HTTPException(status_code=404, detail="User Not Found")
+
+
 """
 Puntos a calificar - API REST:
 
